5f13a21502217eab2a1b67bf76ac5c6db988f936
Reward Steve <rewardstephen30@gmail.com>
2025-01-10 12:43:44 +0100

    added-functionality

--- app.js
+++ app.js
@@ -1,37 +1,80 @@
 const currYear = document.getElementById("year");
 const selectImage = document.querySelector(".select-file");
 const img = document.querySelector("#upload-image img");
-const user = document.querySelectorAll("#inputs");
+const userName = document.querySelector(".input-name");
+const userEmail = document.querySelector(".input-email");
+const userGitHub = document.querySelector(".input-github");
 const submitBtn = document.querySelector(".btn");
+const picInfo = document.getElementById("info-message");
+const formDoc = document.querySelector("form");
 
 currYear.textContent = new Date().getFullYear();
-
 const form = [];
 
 const addProfilePic = (e) => {
   let files = e.target.files[0];
   img.src = URL.createObjectURL(files);
+  picInfo.style.opacity = 0;
 };
 
-const updateForm = () => {
-  let Name = user[0].value;
-  let email = user[1].value;
-  let gitname = user[2].value;
+//CAPITALIZE FIRST LETTER
+
+const capitalizeFirstLetter = (string) => {
+  return string
+    .toLowerCase()
+    .split(" ")
+    .map((str) => str[0].toUpperCase() + str.slice(1))
+    .join(" ");
+};
+
+//EMAIL VALIDATION
+const validateEmail = (email) => {
+  const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regEx.test(String(email).toLowerCase());
+};
+
+const updateForm = (e) => {
+  e.preventDefault();
+  let Name = capitalizeFirstLetter(userName.value.trim().toUpperCase());
+  let email = userEmail.value.trim();
+  let gitname = userGitHub.value.trim();
   let picture = img.src;
 
-  if ((Name !== "" && email !== "" && gitname !== "", picture !== "")) {
-    const formdetails = {
-      id: Math.round(Math.random() * 896000),
-      profilePic: picture,
-      userName: Name,
-      userEmail: email,
-      gitHub: gitname,
-    };
-
-    form.push(formdetails);
-    localStorage.setItem("item", JSON.stringify(form));
+  //Warning if all input fields are empty
+  if (Name === "" && email === "" && gitname === "") {
+    alert("All fields are required.");
+    return;
   }
+
+  //Warning if email address is not valid
+  if (!validateEmail(email)) {
+    alert("Please enter a valid email address");
+    return;
+  }
+
+  //Warning if profile picture is empty
+  if (picture === "") {
+    alert("Please upload a profile picture.");
+    return;
+  }
+
+  const formdetails = {
+    id: Math.round(Math.random() * 766680),
+    profilePic: picture,
+    userName: Name,
+    userEmail: email,
+    gitHub: gitname,
+  };
+
+  form.push(formdetails);
+  localStorage.setItem("item", JSON.stringify(form));
+  window.location.href = "./preview.html";
 };
 
 submitBtn.addEventListener("click", updateForm);
 selectImage.addEventListener("change", addProfilePic);
+
+// Accessibility: Allow form submission with Enter key
+formDoc.addEventListener("keydown", (e) => {
+  e.key === "Enter" ? updateForm(e) : "";
+});
--- preview.js
+++ preview.js
@@ -5,23 +5,54 @@ const id = document.getElementById("id");
 const profilePic = document.querySelector(".avatar");
 const currEvent = document.getElementById("current-event");
 
-const response = localStorage.getItem("item");
-const data = JSON.parse(response);
-
-if (
-  data[0].id !== "" &&
-  data[0].userName !== "" &&
-  data[0].userEmail !== "" &&
-  data[0].profilePic !== "" &&
-  data[0].gitHub !== ""
-) {
-  userName.forEach((name) => {
-    name.textContent = data[0].userName;
-  });
-  userEmail.textContent = data[0].userEmail;
-  gitName.textContent = data[0].gitHub;
-  profilePic.src = data[0].profilePic;
-  id.textContent = `#${data[0].id}`;
+async function getFromStorage() {
+  try {
+    const response = localStorage.getItem("item");
+    if (response) {
+      const data = await JSON.parse(response);
+      if (
+        data[0].id !== "" &&
+        data[0].userName !== "" &&
+        data[0].userEmail !== "" &&
+        data[0].profilePic !== "" &&
+        data[0].gitHub !== ""
+      ) {
+        userName.forEach((name) => {
+          name.textContent = data[0].userName;
+        });
+        userEmail.textContent = data[0].userEmail;
+        gitName.textContent = data[0].gitHub;
+        profilePic.src = data[0].profilePic;
+        id.textContent = `#${data[0].id}`;
+      }
+    }
+  } catch (err) {
+    console.log(`Slow network: ${err}`);
+  }
 }
+getFromStorage();
 
-console.log(id);
+// Set a default image if the image is not found
+profilePic.addEventListener("error", () => {
+  profilePic.src = "./assets/images/image-avatar.jpg";
+});
+
+const arrMonth = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const date = new Date();
+currEvent.textContent = `${
+  arrMonth[date.getMonth()]
+} ${date.getDate()}, ${date.getFullYear()} / Ojo Lagos`;
